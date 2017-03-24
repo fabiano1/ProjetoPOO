12bb7b400f50de8831f8cac06c375b3200634288
fabiano1 <fabiano.ferreira@dce.ufpb.br>
2017-03-24 10:31:25 -0300

    locar veiculo apenas se estiver veiculo

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -15,31 +15,31 @@ public class Principal implements Serializable{
 	/*private static ColecaoPessoa pessoa = new ColecaoPessoa();
 	private static ColecaoVeiculo veiculo = new ColecaoVeiculo();*/
 	private static ColecaoLocacao locacao = new ColecaoLocacao();
-	
+
 	public static void main(String[] args) throws Exception {
-		
+
 		int op = 0, i=0;
 		Scanner sc = new Scanner(System.in);
 		String str = new String();
 		Endereco endereço = new Endereco();
-		
+
 		/*do{
 			System.out.println("Menu ");
 			System.out.println("0- Sair");
 			System.out.println("1- Adicionar Pessoa");
 			System.out.println("2- Listar Pessoal");
 			System.out.println("3- Listar Clientes");
-			
+
 		}while(op!=0);*/
-		
+
 		if(!ic.init()){
 			System.out.println("Arquivo não foi carregado");
 		}
-		
+
 		do{/*
 			System.out.println("Menu ");
 			System.out.println("0- Sair");
-			
+
 			System.out.println("\nPessoa:");
 			System.out.println("1- Adicionar Pessoa");
 			System.out.println("2- Listar Pessoal");
@@ -47,7 +47,7 @@ public class Principal implements Serializable{
 			System.out.println("4- Listar Funcionarios");
 			System.out.println("5- Pesquisar Pessoa");
 			System.out.println("6- Remover Pessoa");
-			
+
 			System.out.println("\nVeiculo:");
 			System.out.println("7- Adicionar Veiculo");
 			System.out.println("8- Listar Veiculos");
@@ -56,7 +56,7 @@ public class Principal implements Serializable{
 			System.out.println("11- Listar Motocicletas");
 			System.out.println("12- Procurar Veiculo");
 			System.out.println("13- Remover Veiculo");
-			
+
 			System.out.println("\nLocacao:");
 			System.out.println("14- Adiconar Locacao");
 			System.out.println("15- Listar Locacoes");
@@ -64,130 +64,130 @@ public class Principal implements Serializable{
 			System.out.println("17- Procurar Locacao por Pessoa");
 			System.out.println("18- Procurar Locacao por veiculo");
 			System.out.println("19- Remover Locacao");
-		*/
+		 */
 			System.out.println("___________________________________________________________________________________________________\n|"
 					+" |                                        Menu                                                    |");
-				System.out.println("|\n| Pessoa:                        Veiculo:                        Locacao:                         |\n|                                                                                                 |\n|"
-							+" 1- Adicionar Pessoa            7- Adicionar Veiculo             14- Adiconar Locacao            |\n|"                                   
-							+" 2- Listar Pessoal              8- Listar Veiculos               15- Listar Locacoes             |\n|"
-							+" 3- Listar Clientes             9- Listar Veiculos Disponiveis   16- Procurar Locacao por ID     |\n|"
-							+" 4- Listar Funcionarios         10-Listar Automoveis             17- Procurar Locacao por Pessoa |\n|"
-							+" 5- Pesquisar Pessoa            11-Listar Motocicletas           18- Procurar Locacao por veiculo|\n|"
-							+" 6- Remover Pessoa              12-Procurar Veiculo              19- Remover Locacao             |\n"
-							+"|                                13-Remover Veiculo                                               |\n"
-							+"|                                                                                                 |\n|                                                                                                 |\n|"
-							+"                              0- Sair                                                            |\n|"   
-							+ "                                                                                                 |\n|"
-							+"_________________________________________________________________________________________________|");
+			System.out.println("|\n| Pessoa:                        Veiculo:                        Locacao:                         |\n|                                                                                                 |\n|"
+					+" 1- Adicionar Pessoa            7- Adicionar Veiculo             14- Adiconar Locacao            |\n|"                                   
+					+" 2- Listar Pessoal              8- Listar Veiculos               15- Listar Locacoes             |\n|"
+					+" 3- Listar Clientes             9- Listar Veiculos Disponiveis   16- Procurar Locacao por ID     |\n|"
+					+" 4- Listar Funcionarios         10-Listar Automoveis             17- Procurar Locacao por Pessoa |\n|"
+					+" 5- Pesquisar Pessoa            11-Listar Motocicletas           18- Procurar Locacao por veiculo|\n|"
+					+" 6- Remover Pessoa              12-Procurar Veiculo              19- Remover Locacao             |\n"
+					+"|                                13-Remover Veiculo                                               |\n"
+					+"|                                                                                                 |\n|                                                                                                 |\n|"
+					+"                              0- Sair                                                            |\n|"   
+					+ "                                                                                                 |\n|"
+					+"_________________________________________________________________________________________________|");
 
 
 			System.out.print("\nOpção: ");
 			op = lerInteiro();
-			
+
 			switch(op){
-			
-				/*Pessoa*/
-				case 1:
-					if(adicionarPessoa()){
-						System.out.println("pessoa adicionada com sucesso"); 
-					}
-					break;
-			
-				case 2:
-					System.out.println(ic.listarPessoa().toString());
-					break;
-					
-				case 3:
-					System.out.println(ic.listarCliente().toString());
-					break;
-					
-				case 4:
-					System.out.println(ic.listarFuncionario().toString());
-					break;
-					
-				case 5:
-					System.out.print("digite o documento: ");
-					str = lerDocumento();
-					System.out.println(ic.pesquisarPessoaPeloCPF(str));
-					break;
-					
-				case 6:
-					System.out.print("digite o documento: ");
-					str = lerDocumento();
-					ic.removerPessoa(str);
-					break;
-					
+
+			/*Pessoa*/
+			case 1:
+				if(adicionarPessoa()){
+					System.out.println("pessoa adicionada com sucesso"); 
+				}
+				break;
+
+			case 2:
+				System.out.println(ic.listarPessoa().toString());
+				break;
+
+			case 3:
+				System.out.println(ic.listarCliente().toString());
+				break;
+
+			case 4:
+				System.out.println(ic.listarFuncionario().toString());
+				break;
+
+			case 5:
+				System.out.print("digite o documento: ");
+				str = lerDocumento();
+				System.out.println(ic.pesquisarPessoaPeloCPF(str));
+				break;
+
+			case 6:
+				System.out.print("digite o documento: ");
+				str = lerDocumento();
+				ic.removerPessoa(str);
+				break;
+
 				/*Veiculo*/
-				case 7:
-					if(adicionarVeiculo())
-					{
-						System.out.println("Veiculo adicionado com sucesso");
-					}
-					break;
-				case 8:
-					System.out.println(ic.listarVeiculos().toString());
-					break;
-				case 9:
-					System.out.println(ic.listarVeiculosDisponiveis().toString());
-					break;
-				case 10:
-					System.out.println(ic.listarAutomoveis().toString());
-					break;
-				case 11:
-					System.out.println(ic.listarMotocicletas().toString());
-					break;
-				case 12:
-					System.out.print("digite o numero da Placa: ");
-					str = sc.nextLine();
-					System.out.println(ic.pesquisarVeiculo(str));
-					break;
-				case 13:
-					System.out.print("digite o numero da Placa: ");
-					str = sc.nextLine();
-					ic.removerVeiculo(str);
+			case 7:
+				if(adicionarVeiculo())
+				{
+					System.out.println("Veiculo adicionado com sucesso");
+				}
+				break;
+			case 8:
+				System.out.println(ic.listarVeiculos().toString());
+				break;
+			case 9:
+				System.out.println(ic.listarVeiculosDisponiveis().toString());
+				break;
+			case 10:
+				System.out.println(ic.listarAutomoveis().toString());
+				break;
+			case 11:
+				System.out.println(ic.listarMotocicletas().toString());
+				break;
+			case 12:
+				System.out.print("digite o numero da Placa: ");
+				str = sc.nextLine();
+				System.out.println(ic.pesquisarVeiculo(str));
+				break;
+			case 13:
+				System.out.print("digite o numero da Placa: ");
+				str = sc.nextLine();
+				ic.removerVeiculo(str);
 				break;
 				/*Locacao*/
-				case 14:
-					if(adicionarLocacao())
-					{
-						System.out.println("Locacao adicionado com sucesso");
-					}
-					break;
-				case 15:
-					System.out.println(ic.listarLocacoes().toString());
-					break;
-				case 16:
-					System.out.print("digite o id da locacao: ");
-					i = lerInteiro();
-					System.out.println(ic.pesquisarLocacao(i));
-					break;
-				case 17:
-					System.out.print("digite o cpf do cliente: ");
-					str = lerDocumento();
-					//System.out.println(ic.pesquisarLocacao(ic.pesquisarPessoaPeloCPF(str)));
-					break;
-				case 18:
-					System.out.print("digite o numero da  placa do veiculo: ");
-					str = sc.nextLine();
+			case 14:
+				if(adicionarLocacao())
+				{
+					System.out.println("Locacao adicionado com sucesso");
+				}
+				break;
+			case 15:
+				System.out.println(ic.listarLocacoes().toString());
+				break;
+			case 16:
+				System.out.print("digite o id da locacao: ");
+				i = lerInteiro();
+				System.out.println(ic.pesquisarLocacao(i));
+				break;
+			case 17:
+				System.out.print("digite o cpf do cliente: ");
+				str = lerDocumento();
+				//System.out.println(ic.pesquisarLocacao(ic.pesquisarPessoaPeloCPF(str)));
+				break;
+			case 18:
+				System.out.print("digite o numero da  placa do veiculo: ");
+				str = sc.nextLine();
 				//	System.out.println(ic.pesquisarLocacao(ic.pesquisarVeiculo(str)));
-					break;
-				case 19:
-					System.out.print("digite o id da locacao: ");
-					i = lerInteiro();
-					ic.removerLocacao(i);
-					break;
+				break;
+			case 19:
+				System.out.print("digite o id da locacao: ");
+				i = lerInteiro();
+				ic.removerLocacao(i);
+				break;
 			}
 		}while(op!=0);
 	}
-		
+
 	public static boolean adicionarPessoa() throws Exception{
 		int op = 0;
 		Scanner sc = new Scanner(System.in);
 		String str = new String();
 		Cliente client = new Cliente();
 		Funcionario func = new Funcionario();
-	
-		
+
+
 		do{
 			System.out.println("Escolha o tipo de Pessoa");
 			System.out.println("1 - Cliente");
@@ -195,7 +195,7 @@ public class Principal implements Serializable{
 			System.out.print("opcao: ");
 			op = lerInteiro();
 		}while((op!=1)&&(op!=2));
-		
+
 		if(op==1){
 			lerDadosPessoa(client);
 			System.out.print("digite a cnh:");
@@ -205,10 +205,10 @@ public class Principal implements Serializable{
 			sc.nextLine();
 			str = sc.nextLine();
 			client.setCategoriaCnh(str);
-			
-		//	System.out.println(client);
+
+			//	System.out.println(client);
 			ic.addPessoa(client);
-		//	System.out.println(client);
+			//	System.out.println(client);
 			return true;
 		}
 		if(op==2){
@@ -219,7 +219,7 @@ public class Principal implements Serializable{
 			ic.addPessoa(func);
 			return true;
 		}
-		
+
 		return false;
 	}
 
@@ -228,34 +228,34 @@ public class Principal implements Serializable{
 		Scanner sc = new Scanner(System.in);
 		Endereco endereco = new Endereco();
 		String data = new String();
-		
+
 		System.out.print("digite o cpf da pessoa: ");
 		str = lerDocumento();
-	/*	if(ic.pesquisarPessoaPeloCPF(str)!= null)
+		/*	if(ic.pesquisarPessoaPeloCPF(str)!= null)
 		{
 			System.out.println("pessoa já está cadastrado");
 			return false;
 		}*/
 		p.setCpf(str);
-		
+
 		System.out.print("digite o nome da pessoa: ");
 		str = lerNome();
 		p.setNome(str);
-		
+
 		System.out.print("digite a data de Nascimento: ");
 		str = lerData();
-	//	p.setDataNascimento(str);
-		
+		//	p.setDataNascimento(str);
+
 		System.out.print("digite o login: ");
 		str = sc.nextLine();
 		p.setLogin(str);
-		
+
 		System.out.print("digite a senha: ");
 		str = sc.nextLine();
 		p.setSenha(str);
-		
+
 		System.out.print("digite o endereço da pessoa: ");
-	
+
 		System.out.print("Cep: ");
 		str = sc.nextLine();
 		endereco.setCep(str);
@@ -271,20 +271,20 @@ public class Principal implements Serializable{
 		System.out.print("digite o Estado: ");
 		str = sc.nextLine();
 		endereco.setEstado(str);
-		
+
 		p.setEndereco(endereco);
 		return true;
-		
+
 	}
-	
+
 	public static boolean adicionarVeiculo() throws Exception{
 		int op = 0, i = 0;
 		Scanner sc = new Scanner(System.in);
 		String str = new String();
 		Automovel automovel= new Automovel();
 		Motocicleta motocicleta = new Motocicleta();
-	
-		
+
+
 		do{
 			System.out.println("Escolha o tipo de Veiculo");
 			System.out.println("1 - Automovel");
@@ -292,7 +292,7 @@ public class Principal implements Serializable{
 			System.out.print("opcao: ");
 			op = sc.nextInt();
 		}while((op!=1)&&(op!=2));
-		
+
 		if(op==1){
 			lerDadosVeiculo(automovel);
 			System.out.println("digite a quantidade de portas:");
@@ -309,7 +309,7 @@ public class Principal implements Serializable{
 			ic.addVeiculo(motocicleta);
 			return true;
 		}
-		
+
 		return false;
 	}
 
@@ -319,15 +319,15 @@ public class Principal implements Serializable{
 		Placa placa = new Placa();
 		String data = new String();
 		double d = 0;
-	
-	/*	if(ic.pesquisarVeiculo(str)!=null)
+
+		/*	if(ic.pesquisarVeiculo(str)!=null)
 		{
 			System.out.println("veiculo já existe");
 			return false;
 		}*/
-		
+
 		System.out.print("digite a placa: ");
-		
+
 		System.out.print("Numero: ");
 		str = sc.nextLine();
 		placa.setNumerosEletras(str);
@@ -337,26 +337,26 @@ public class Principal implements Serializable{
 		System.out.print("Estado: ");
 		str = sc.nextLine();
 		placa.setEstado(str);
-		
+
 		v.setPlaca(placa);
 
 		System.out.print("digite a marca: ");
 		str = sc.nextLine();
 		v.setMarca(str);
-		
+
 		System.out.print("digite o Modelo: ");
 		str = sc.nextLine();
 		v.setModelo(str);
-		
+
 		System.out.print("digite o preco por hora ");
 		d = lerDouble();
 		v.setPrecoPorHra(d);
-		
-			
+
+
 		return true;
-		
+
 	}
-	
+
 	public static boolean adicionarLocacao() throws Exception{
 		String str;
 		Scanner sc = new Scanner(System.in);
@@ -364,11 +364,11 @@ public class Principal implements Serializable{
 		String data = new String();
 		Locacao locacao = new Locacao();
 		double d = 0;
-		
+
 		System.out.print("digite a data de devolucao: ");
 		data = lerData();
-	//	locacao.setDataRetirada(data);
-		
+		//	locacao.setDataRetirada(data);
+
 		System.out.print("digite o cpf do cliente: ");
 		str = lerDocumento();
 		if(ic.pesquisarPessoaPeloCPF(str) instanceof Cliente){
@@ -376,17 +376,33 @@ public class Principal implements Serializable{
 		}else{
 			return false;
 		}
-		
-		System.out.print("digite o numero da placa do veiculo: ");
-		str = sc.nextLine();
-		locacao.setVeiculo(ic.pesquisarVeiculo(str));
-		
+
+		/**
+		 * Verifica se um veículo está disponível para locação
+		 */
+		boolean v=false;
+		while(v){
+			System.out.print("digite o numero da placa do veiculo: ");
+			str = sc.nextLine();
+
+			if(!ic.pesquisarVeiculo(str).isDisponibilidade()){
+				System.out.println(ic.pesquisarVeiculo(str).toString()+ "Não está Disponível!\n");
+				
+				
+			}else{
+				locacao.setVeiculo(ic.pesquisarVeiculo(str));
+				ic.pesquisarVeiculo(str).setDisponibilidade(false);
+				v=true;
+			}
+
+		}
+
 		return true;
 	}
-	
+
 	public static int lerInteiro(){
 		Scanner sc = new Scanner(System.in);
-		
+
 		if( !sc.hasNextInt())
 		{
 			sc.next();
@@ -395,10 +411,10 @@ public class Principal implements Serializable{
 		}
 		return sc.nextInt();
 	}
-	
+
 	public static double lerDouble(){
 		Scanner sc = new Scanner(System.in);
-		
+
 		if( !sc.hasNextDouble())
 		{
 			sc.next();
@@ -407,10 +423,10 @@ public class Principal implements Serializable{
 		}
 		return sc.nextDouble();
 	}
-	
+
 	public static String lerDocumento(){
 		Scanner sc = new Scanner(System.in);
-		
+
 		if( !sc.hasNextInt())
 		{
 			sc.next();
@@ -419,7 +435,7 @@ public class Principal implements Serializable{
 		}
 		return sc.nextLine();
 	}
-	
+
 	public static String lerNome(){
 		Scanner sc = new Scanner(System.in);
 		if( sc.hasNextDouble()  ||  sc.hasNextInt() )
@@ -430,12 +446,12 @@ public class Principal implements Serializable{
 		}
 		return sc.nextLine();
 	}
-	
+
 	public static String lerData()throws ParseException{
 		Scanner sc = new Scanner(System.in);
 		String leitor, res;
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		leitor = sc.nextLine();
 		if(sdf.parse(leitor)==null)
 		{
@@ -444,7 +460,7 @@ public class Principal implements Serializable{
 		}
 		return leitor;
 	}
-	
+
 	/*
 	 * public static String lerData(Scanner in){
 		System.out.print("Digite no formato (dd/mm/aaaa) ");
@@ -464,10 +480,10 @@ public class Principal implements Serializable{
 		LocalDate sData = LocalDate.parse( lerData(sc) , fmt2 );	
 		return sData;
 	}
-	
+
 	 */
-	
-	
+
+
 	/**
 	 * 
 	 */
@@ -500,22 +516,22 @@ public class Principal implements Serializable{
 		ic.addPessoa(c1);
 
 		System.out.println(ic.listarPessoa().toString());
-		
+
 		for (Cliente cli : ic.listarCliente()) {
-			
+
 			if(cli.getCnh().equals("111")){
 				System.out.println(cli.toString());
 			}
-			
+
 		}
 		System.out.println("Removendo..");
 		ic.removerPessoa("123");
 		ic.removerPessoa("000");
 		System.out.println(ic.listarPessoa().toString());
 
-		
+
 		/*
-		
+
 		ic.listarCliente("123");
 		ic.listarCliente("111");
 		ic.listarCliente("123");
