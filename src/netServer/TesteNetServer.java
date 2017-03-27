cbb0ee1f5db19cbacccd22cd266258c39712dca3
andersonsaleslima <a.sales.lima97@gmail.com>
2017-03-27 17:10:31 -0300

    Implementacao de Socket

---
+++ src/netServer/TesteNetServer.java
@@ -0,0 +1,13 @@
+package netServer;
+
+public class TesteNetServer {
+
+	private static INetServer iNet = new ControllerNetServer();
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		iNet.initServer();
+
+	}
+
+}
